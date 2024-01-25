07eeb2ad7c82c1ce905ae19ea44606c919a6a677
AjMing <62439174+AjMing@users.noreply.github.com>
2024-01-25 23:17:12 +0700

    Update and rename main.c to main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  First arg
+//
+//  Created by Mingmanas Sivaraksa on 2/1/2567 BE.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    
+ 
+  cout<<"argc is "<<argc<<endl;
+    int i,N;
+    for(i=1;i<argc;i++){
+        cout<<"argv[" << i<<"]" <<argv[i]<<endl;
+      
+    }
+    
+    return 0;
+}
